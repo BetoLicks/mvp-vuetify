8e03f11b7cdf6a8f1951e34c4a7a758af7e72c6e
Beto Licks <betaolicks@gmail.com>
2025-11-18 10:31:18 -0300

    fix: adjust login page

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <ContainerDefault>
+    <v-row no-gutters class="h-100">
+      <v-col class="d-flex align-center justify-center bg-grey-lighten-4" cols="12" md="6">
+        1
+
+      </v-col>
+
+      <v-col class="d-flex align-center justify-center" cols="12" md="6">
+        <div class="w-100" style="max-width: 400px;">
+          <v-form>
+            <div class="text-center mb-6">
+              <h2>Login</h2>
+              <p>Please enter your credentials to login.</p>
+
+            </div>
+
+          </v-form>
+        </div>
+      </v-col>
+    </v-row>
+  </ContainerDefault>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,24 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [{
+  routes: [
+    {
+       path: '/',
+       component: () => import('@/layouts/LoginLayout.vue'),
+       children: [
+          {
+            path: '',
+            name: 'login',
+            component: () => import('@/pages/LoginPage.vue'),
+          },
+       ]
+    },
+    {
     path: '/',
     component: () => import('@/layouts/DefaultLayout.vue'),
     children: [
       {
-        path: '',
+        path: 'home',
         name: 'home',
         component: () => import('@/pages/HomePage.vue'),
       },
